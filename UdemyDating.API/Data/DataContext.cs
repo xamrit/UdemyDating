9e57dadff8800a942f5d32fcea5824a66585a16e
AKhalsa <akhalsa@live.com>
2020-07-26 21:05:10 -0400

    Initial Commit

---
+++ UdemyDating.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UdemyDating.API.Models;
+
+namespace UdemyDating.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
